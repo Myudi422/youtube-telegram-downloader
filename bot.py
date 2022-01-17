271453bd7f1aced3db74cdcdaab039ed1c2f80c6
Myudi422 <43848105+Myudi422@users.noreply.github.com>
2022-01-17 11:19:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,9 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-HEROKU = os.getenv('HEROKU')
-PORT = os.getenv('PORT', 5000)
+BOT_TOKEN = os.getenv('BOT_TOKEN' 1920905087:AAGvlh_lBs44Z_s9l7IzNyxtlo040oVKNFY)
+HEROKU = os.getenv('HEROKU' 103.174.115.181)
+PORT = os.getenv('PORT', 443)
 
 # error if there is no bot token set
 if None in (BOT_TOKEN, HEROKU):
